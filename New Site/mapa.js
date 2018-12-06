f28b1ebe4895e08c1e41f98144d2d396ecd0acf4
Daniela-Rodrigues <danielarodrigues.to@gmail.com>
2018-12-06 02:05:41 +0000

    infowindow mapa

--- New Site/mapa.js
+++ New Site/mapa.js
@@ -1,20 +1,26 @@
 function myMap() {
-
-    const myLocation = new google.maps.LatLng(41.552149, -8.421033)
+//definir localização cidade de Braga
+    const myLocation = new google.maps.LatLng(41.550197, -8.426933)
 
     const mapProp = {
         center: myLocation,
-        zoom: 20,
+        zoom: 17,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
     const map = new google.maps.Map(
         document.getElementById("googleMap"), 
         mapProp)
 
+        //janela de informação
+        const infowindow = new google.maps.InfoWindow({content:"Equipa Desenvolvimento Infantil em Braga - Contacte-nos!"})
         const marker = new google.maps.Marker({
             position:myLocation,
+            map:map,
             animation:google.maps.Animation.BOUNCE
-            });
+            })
           
+            marker.addListener('click', function(){
+                infowindow.open(map, marker)
+            })
           marker.setMap(map);
-}
+        }
