4a68feb03153d8e554ec025b6494f16907ab2e17
Daniela-Rodrigues <danielarodrigues.to@gmail.com>
2018-12-06 17:18:53 +0000

    .

--- New Site/mapa.js
+++ New Site/mapa.js
@@ -13,9 +13,9 @@ function myMap() {
 
 
     
-        var contentString = '<div id="content"><div id="siteNotice"></div>'+
-        '<h3 id="firstHeading" class="firsttHeading">Equipa Desenvolvimento Infantil em Braga - Contacte-nos</h1>'
-        var infowindow = new google.maps.InfoWindow({ content: contentString, maxWidth: 450 });
+        var contentString = '<div id="content"><div id="siteNotice"></div>'+ '<h4 >Equipa Desenvolvimento Infantil em Braga!<br> Contacte-nos</h4>'
+
+        var infowindow = new google.maps.InfoWindow({ content: contentString, maxWidth: 335,});
         const marker = new google.maps.Marker({
             position:myLocation,
             map:map,
